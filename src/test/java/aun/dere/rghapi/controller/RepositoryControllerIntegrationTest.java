ae29c6483d0d2d9d031b4b15b7f84dee295c42a8
aundere <58791453+aundere@users.noreply.github.com>
2025-07-24 05:21:35 +0200

    Add WireMock to integration tests

--- src/test/java/aun/dere/rghapi/controller/RepositoryControllerIntegrationTest.java
+++ src/test/java/aun/dere/rghapi/controller/RepositoryControllerIntegrationTest.java
@@ -2,36 +2,29 @@ package aun.dere.rghapi.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.wiremock.spring.EnableWireMock;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@AutoConfigureMockMvc
+@EnableWireMock
+@AutoConfigureWebTestClient
+@TestPropertySource(properties = {
+        "github.api.url=http://localhost:${wiremock.server.port}",
+})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RepositoryControllerIntegrationTest {
 
     @Autowired
-    private MockMvc mvc;
+    private WebTestClient webTestClient;
 
     @Test
-    public void shouldReturnRepositoriesForValidUser() throws Exception {
-        mvc.perform(get("/repositories/octocat"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(6)) // octocat has 6 public not forked repos
-                .andExpect(jsonPath("$[0].owner").value("octocat"))
-                .andExpect(jsonPath("$[0].name").isNotEmpty())
-                .andExpect(jsonPath("$[0].branches[0].name").isNotEmpty())
-                .andExpect(jsonPath("$[0].branches[0].commitSha").isNotEmpty())
-                .andExpect(jsonPath("$[?(@.name == 'linguist')]").doesNotExist()) // linguist is a forked repo
-                .andExpect(jsonPath("$[?(@.name == 'Spoon-Knife')]").exists())
-                .andExpect(jsonPath("$[?(@.name == 'Spoon-Knife')].branches.length()").value(3)) // Spoon-Knife has 3 branches
-                .andExpect(jsonPath("$[?(@.name == 'Spoon-Knife')].branches[?(@.name == 'change-the-title')].commitSha").exists())
-                .andExpect(jsonPath("$[?(@.name == 'Spoon-Knife')].branches[?(@.name == 'change-the-title')].commitSha")
-                        .value("f439fc5710cd87a4025247e8f75901cdadf5333d"));
-
+    public void shouldReturnRepositoriesForValidUser() {
+        this.webTestClient.get()
+                .uri("/repositories/user")
+                .exchange()
+                .expectBody()
+                .json("[{owner: 'user', name:  'test-repo', branches: [{name: 'master', commitSha: '123abc'}]}]");
     }
 }
